880006d6e4ded18a800f6cc27efe18e265ac2f08
Filipe Queiroz <filipe.queiroz.abreu@gmail.com>
2026-03-14 23:05:20 -0300

    fixing outputs

--- src/infrastructure/outputs.tf
+++ src/infrastructure/outputs.tf
@@ -1,3 +1,8 @@
 output lambda_id {
   value       = aws_lambda_function.selfpsico.id
 }
+
+output api_gateway_url {
+  value       = aws_api_gateway_rest_api.selfpsico.invoke_url
+  description = "API Gateway URL"
+}
