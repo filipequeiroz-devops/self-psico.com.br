91429885590153bccc479d20d3419c954700d11a
Filipe Queiroz <filipe.queiroz.abreu@gmail.com>
2026-03-15 18:56:20 -0300

    output s3

--- src/infrastructure/outputs.tf
+++ src/infrastructure/outputs.tf
@@ -6,3 +6,8 @@ output api_gateway_url {
   value       = aws_api_gateway_rest_api.selfpsico.invoke_url
   description = "API Gateway URL"
 }
+
+output s3_bucket_name {
+  value       = aws_s3_bucket.selfpsico.bucket
+  description = "S3 Bucket Name"
+}
